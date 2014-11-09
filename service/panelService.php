9ff9db76e21c4b463383054e8bade2ddd2b70e25
predorock <predo@jarvis.homenet.telecomitalia.it>
2014-11-09 20:07:30 +0100

    XHR send fix on backend.

--- service/panelService.php
+++ service/panelService.php
@@ -5,10 +5,11 @@
  *  - In attessa di una query
  *   - La query restituisce l'elenco dei prodotti legati ad un gruppo da'acquisto relativi ad un ordine
  */
-
-include_once('sample_db.php');
+include 'sample_common.php';
+header('Content-type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
 /*
  * Query -> gruppo, ordine -> prodotti
  */
-if(isset($_POST['q'])) echo $_POST['q'];
+if(isset($_POST['q'])) echo "{'q' : ".$_POST['q']."}";
 ?>
